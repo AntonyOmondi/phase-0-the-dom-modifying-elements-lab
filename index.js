3729353e3de8d516b70a2ee06928f66b48e834ec
Antony Omondi <tonymboya13@gmail.com>
2025-04-12 13:05:13 +0300

    complete dDOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+newHeader.textContent = "Antony Omondi is the champion"
